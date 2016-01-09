63230c1ddc79347c8ab3590bcb8c6be100e02998
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2016-01-09 18:51:57 -0500

    cleanup

--- build/Release/.deps/Release/zorba.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/zorba.node := ./gyp-mac-tool flock ./Release/linker.lock c++ -bundle -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/zorba.node Release/obj.target/zorba/src/NativeExtension.o Release/obj.target/zorba/src/functions.o -undefined dynamic_lookup /opt/local/lib/libzorba_simplestore.dylib
